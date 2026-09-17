1650a047a21a4b800dd687f28dc0540e3ceeedd2
wzh411 <13611334175@163.com>
2026-09-17 09:42:18 +0800

    头文件

---
+++ src/publisher_sin.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64.hpp>
+using namespace std;
+using namespace std::chrono_literals; 
+int main(int argc, char*argv[]){
+  return 0;
+}
