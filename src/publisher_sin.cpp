e3882545dcaae66d388f38ff305c7547fe61699e
wzh411 <13611334175@163.com>
2026-09-17 09:44:01 +0800

    jiedian

--- src/publisher_sin.cpp
+++ src/publisher_sin.cpp
@@ -4,5 +4,13 @@
 using namespace std;
 using namespace std::chrono_literals; 
 int main(int argc, char*argv[]){
+  rclcpp::init(argc,argv);
+  auto n = std::make_shared<rclcpp::Node>("bo_publisher");
+  auto p=n->create_publisher<std_msgs::msg::Float64>("bo",10);
+  
+  
+  
+  
+  
   return 0;
 }
