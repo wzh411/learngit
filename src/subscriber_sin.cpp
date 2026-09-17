be12c4bdd65172b935bb52241c99e0576dc27686
wzh411 <13611334175@163.com>
2026-09-17 21:55:36 +0800

    media滤波器生成

--- src/subscriber_sin.cpp
+++ src/subscriber_sin.cpp
@@ -9,9 +9,20 @@ class Subscriber_sin : public rclcpp::Node{
         auto topic =
          [this](std_msgs::msg::Float64::UniquePtr msg) -> void{
             double raw =msg->data;
-            double low = (lastlow) +(alpha_) *(raw-lastlow);
-            lastlow =low;
-          RCLCPP_INFO(this->get_logger(), "", msg->data);  
+            
+            double lowpass = (lastlow) +(alpha_) *(raw-lastlow);
+            lastlow =lowpass;
+
+            media .push_back(raw);
+            if(media .size() >5){
+                media erase(media .begin());
+            }
+            vector<double> t =media;
+            sort (t.begin(),t.end());
+            double m = t[t.size()/2];
+
+            
+        RCLCPP_INFO(this->get_logger(), "", msg->data);  
         
         };
     subscription_ =
@@ -21,6 +32,7 @@ class Subscriber_sin : public rclcpp::Node{
     rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_;
     double alpha_;           
     double lastlow;
+    vector <double> media;
 };
 int main(int argc,char*argv[]){
     rclcpp::init(argc,argv);
