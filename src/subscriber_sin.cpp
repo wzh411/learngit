3ad6d56b5df508edbb0f9a93f6fe6a4ded6a94b7
wzh411 <13611334175@163.com>
2026-09-17 21:35:50 +0800

    订阅者节点框架完成

--- src/subscriber_sin.cpp
+++ src/subscriber_sin.cpp
@@ -2,12 +2,24 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float64.hpp>
 using namespace std;
-class Subscriber_sin : public rclcpp::Node
-{
-    public:
-    Subscriber_sin()
-    :Node("subscriber_sin")
-    {
+class Subscriber_sin : public rclcpp::Node{
+    public: 
+    Subscriber_sin():
+    Node("subscriber_sin"){
+        auto topic =
+         [this](std_msgs::msg::Float64::UniquePtr msg) -> void{
+          RCLCPP_INFO(this->get_logger(), "", msg->data);  
         
+        };
+    subscription_ =
+    this->create_subscription<std_msgs::msg::Float64>("sin",10,topic);
     }
+    private:
+    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_;
+};
+int main(int argc,char*argv[]){
+    rclcpp::init(argc,argv);
+    rclcpp::spin(std::make_shared<Subscriber_sin>());
+    rclcpp::shutdown();
+    return 0;
 }
