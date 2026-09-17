919d0bb4225cf103caf8242e03fc44ce9be66252
wzh411 <13611334175@163.com>
2026-09-17 09:45:08 +0800

    命名变量

--- src/publisher_sin.cpp
+++ src/publisher_sin.cpp
@@ -7,7 +7,11 @@ int main(int argc, char*argv[]){
   rclcpp::init(argc,argv);
   auto n = std::make_shared<rclcpp::Node>("bo_publisher");
   auto p=n->create_publisher<std_msgs::msg::Float64>("bo",10);
-  
+  double A = 3.0 , f = 20.0 , pai = M_PI;  
+  random_device rd; 
+  unsigned int s =rd();
+  mt19937 g(s); 
+  normal_distribution <double> no(0.0, 0.01 * A);
   
   
   
