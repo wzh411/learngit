e377a9ee0971a06505c7a09d52ae99849f6cc173
wzh411 <13611334175@163.com>
2026-09-18 13:00:01 +0800

    节点搭建

--- src/dianjikongzhi.cpp
+++ src/dianjikongzhi.cpp
@@ -4,3 +4,20 @@
 #include <motor_sim_msgs/msg/motor_state.hpp>
 using namespace std;
 using namespace std::chrono_literals;
+class Motor : public rclcpp::Node{
+public:
+  Motor() : Node("motor_simulator"), tau(0), w(0), th(0) {
+    sub = create_subscription<std_msgs::msg::Float64>("torque_cmd", 10,
+      [this](std_msgs::msg::Float64::SharedPtr m){ tau = m->data; });
+    pub 
+= create_publisher<motor_sim_msgs::msg::MotorState>("motor_state", 10);
+    timer 
+= create_wall_timer(10ms, [this]{ update(); });
+  }
+
+
+  rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr sub;
+  rclcpp::Publisher<motor_sim_msgs::msg::MotorState>::SharedPtr pub;
+  rclcpp::TimerBase::SharedPtr timer;
+  double tau, w, th;
+  }
