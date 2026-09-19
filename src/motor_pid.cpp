9ea6b144c8cbf02765b339cf9805ecff622a8bd0
wzh411 <13611334175@163.com>
2026-09-19 13:50:31 +0800

    PID的p i d数值 确定最终的输出 完成负反馈调节

--- src/motor_pid.cpp
+++ src/motor_pid.cpp
@@ -6,17 +6,27 @@ using namespace std;
 using namespace std::chrono_literals;
 class Pid : public rclcpp::Node{
     public:
-    Pid():Node("motor_pid"), kp(),ki(),kd(),target(),current(),last_e(),io(),il(),mo(),ib() {
+    Pid():Node("motor_pid"), kp(),ki(),kd(),target(),current(),last_e(),i(),il(),mo(),ib() {
        subscription = create_subscription<motor_sim_msgs::msg::MotorState>("motor_state", 10,[this](motor_sim_msgs::msg::MotorState::SharedPtr m) {
             current = m->av;
         });
        publisher = create_publisher<std_msgs::msg::Float64>("torque_cmd", 10);
        timer = create_wall_timer(2ms, [this] {
-        
+           e = target - current;
+           p = kp * e;
+           if(fabs(e < ib)) i +=ki * e;
+           else i = 0;
+           i = max(-il,min(i,il));
+           d = kd * (e - last_e);
+           out = p + i + d;
+           out = max(-mo,min(mo,out));
+           last_e = e;
+           
+
         });
     }
     private:
-    double kp ,ki,kd,target,current,last_e,io,il,mo,ib,e,p,d,out;
+    double kp ,ki,kd,target,current,last_e,i,il,mo,ib,e,p,d,out;
     rclcpp::Subscription<motor_sim_msgs::msg::MotorState>::SharedPtr subscription;
     rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher;
     rclcpp::TimerBase::SharedPtr timer;
