f0aef37f06dadbdc2fbd5a01691d3768d36480bf
wzh411 <13611334175@163.com>
2026-09-25 20:54:18 +0800

    声明变量 指针 成员

--- rmcs_core/src/hardware/motor_control.cpp
+++ rmcs_core/src/hardware/motor_control.cpp
@@ -25,6 +25,36 @@ namespace rmcs_core::hardware{
     : public rmcs_executor::Component
     , public rclcpp::Node
     , public librmcs::board::RmcsBoardLite::Callback{
+   
+public:
+    Motorcontrol()
+        : Node(
+        get_component_name(),
+        rclcpp::NodeOptions{}.automatically_declare_parameters_from_overrides(true))
+        , logger_(get_logger())
+        , command_(create_partner_component<Command>(get_component_name() + "_command", *this))
+        , motor_(*this, *command_, "/motor")
+        , dr16_{} {
 
+        }
+    
+    private:
+    rclcpp::Logger logger_;
+    std::unique_ptr<librmcs::board::RmcsBoardLite> board_;
+    std::shared_ptr<Command> command_;
+
+    device::DjiMotor motor_;
+    device::Dr16 dr16_;
+    std::unique_ptr<device::RemoteControl> remote_control_;
+
+    PidCalculator speed_pid_;
+    PidCalculator angle_pid_;
+
+    double filtered_velocity_ = 0.0;
+    double motor_command_ = 0.0;
+
+    OutputInterface<double> motor_angle_output_;
+    OutputInterface<double> motor_velocity_output_;
+    OutputInterface<double> motor_command_output_;
     };
 }
