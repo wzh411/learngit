d25254925e2279927490d700ec64f35f3dcee624
wzh411 <13611334175@163.com>
2026-09-25 21:01:26 +0800

    建立类Command

--- rmcs_core/src/hardware/motor_control.cpp
+++ rmcs_core/src/hardware/motor_control.cpp
@@ -27,6 +27,13 @@ namespace rmcs_core::hardware{
     , public librmcs::board::RmcsBoardLite::Callback{
    
 public:
+    class Command : public rmcs_executor::Component {
+public: 
+    explicit Command(Motorcontrol& o) : owner_(o) {}
+    void update() override { owner_.command_update(); }
+    private:
+    Motorcontrol& owner_;
+    };
     Motorcontrol()
         : Node(
         get_component_name(),
@@ -35,6 +42,31 @@ public:
         , command_(create_partner_component<Command>(get_component_name() + "_command", *this))
         , motor_(*this, *command_, "/motor")
         , dr16_{} {
+            motor_.configure(
+            device::DjiMotor::Config{device::DjiMotor::Type::kGM6020, 1}
+                .set_reduction_ratio(1.0));
+        speed_pid_ = PidCalculator{
+            get_parameter("speed_kp").as_double(),
+            get_parameter("speed_ki").as_double(),
+            get_parameter("speed_kd").as_double()};
+        speed_pid_.output_min = -30000;
+        speed_pid_.output_max = 30000;
+
+        angle_pid_ = PidCalculator{
+            get_parameter("angle_kp").as_double(),
+            get_parameter("angle_ki").as_double(),
+            get_parameter("angle_kd").as_double()};
+        angle_pid_.output_min = -10;
+        angle_pid_.output_max = 10;
+
+        register_output("/motor/angle", motor_angle_output_);
+        register_output("/motor/velocity", motor_velocity_output_);
+        register_output("/motor/command", motor_command_output_);
+
+        board_ = std::make_unique<librmcs::board::RmcsBoardLite>(
+            *this, get_parameter("board_serial").as_string());
+        remote_control_ = std::make_unique<device::RemoteControl>(*this);
+        remote_control_->register_dr16(&dr16_);
 
         }
     
